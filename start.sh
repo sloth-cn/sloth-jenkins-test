131db1100ae01de489e9d86f1410038503a8825b
sloth-cn <sloth-cn@outlook.com>
2021-08-04 21:05:40 +0800

    add shell script to test

---
+++ start.sh
@@ -0,0 +1,9 @@
+###
+ # @Author: sloth-cn  sloth-cn@outlook.com
+ # @Date: 2021-08-04 21:04:55
+ # @LastEditTime: 2021-08-04 21:05:08
+ # @LastEditors: sloth-cn  sloth-cn@outlook.com
+ # @Description:
+### 
+
+echo "start to build"
